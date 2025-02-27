7c0f3c9979e033f3a76c279261e870eaf33aaa82
czedrixb <czedrixb@gmail.com>
2025-02-27 22:22:33 +0800

    updated latest changes

---
+++ components/PageHeader.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    class="w-full h-[24rem] sm:h-[19rem] md:h-[16rem] lg:h-[24rem] overflow-hidden relative mb-10"
+  >
+    <video class="w-full h-full object-cover" autoplay loop muted playsinline>
+      <source src="/images/about-us/bg-hero.mp4" type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
+    <div class="absolute inset-0 bg-main opacity-[.60] hidden lg:block"></div>
+    <div class="absolute inset-0 flex justify-center">
+      <img
+        src="/images/about-us/hero-overlay.png"
+        class="max-w-[160%] md:max-w-[96%]"
+        alt="hero-overlay"
+      />
+    </div>
+    <div class="absolute inset-0 flex justify-center items-center">
+      <div>
+        <div
+          class="text-center font-poppins font-[600] text-[25px] lg:text-[40px] md:px-0 text-[#475766] mb-5"
+        >
+          {{ title }}
+        </div>
+
+        <p
+          class="font-inter font-[300] text-[18px] text-center mx-auto text-[#475766] w-[80%] 2xl:w-[60%]"
+        >
+          {{ description }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  title: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+});
+</script>
---
+++ pages/services.vue
@@ -0,0 +1,345 @@
+<template>
+  <div>
+    <PageHeader
+      title="Services"
+      description="Our skilled developers and designers will create a stunning, user-friendly website tailored to your specific company requirements. We ensure an engaging user experience that captivates your audience, from adaptable designs to seamless functionality."
+    />
+
+    <div
+      class="px-5 mx-auto md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl mb-10 overflow-hidden"
+    >
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-3">
+        <div class="flex">
+          <div
+            class="card bg-white shadow-md text-primary-content p-5 pb-0 flex flex-col justify-between"
+          >
+            <div>
+              <div
+                class="font-poppins font-[600] text-[22px] lg:text-[25px] md:px-0 text-[#475766] mb-3"
+              >
+                UI/UX & AI-Powered Solutions
+              </div>
+
+              <p
+                class="text-left font-inter font-[300] text-[18px] text-[#475766] mb-10"
+              >
+                We build high-performing websites and web applications tailored
+                to your business needs. Whether it’s a dynamic website or a
+                scalable web app, we ensure seamless functionality, engaging
+                design, and smooth user experiences that drive growth.
+              </p>
+            </div>
+
+            <div class="flex justify-center">
+              <img
+                src="/images/about-us/services-image1.png"
+                class="max-w-[35%] h-auto md:max-w-[91%] lg:max-w-[66%]"
+                alt="mobile-phone"
+              />
+            </div>
+          </div>
+        </div>
+        <div>
+          <div>
+            <div
+              class="card bg-white shadow-md text-primary-content p-5 pb-0 mb-3"
+            >
+              <div
+                class="font-poppins font-[600] text-[22px] lg:text-[25px] md:px-0 text-[#475766] mb-3"
+              >
+                Website & Web App Development
+              </div>
+
+              <p
+                class="text-left font-inter font-[300] text-[18px] text-[#475766] mb-10"
+              >
+                From intuitive UI design to AI-driven automation, we enhance
+                digital experiences that keep users engaged. Our expertise in
+                design and smart technologies, like chatbots and predictive
+                analytics, helps businesses improve usability, efficiency, and
+                customer satisfaction.
+              </p>
+
+              <img
+                src="/images/about-us/services-image2.png"
+                class="max-w-full h-auto"
+                alt="dashboard-screens"
+              />
+            </div>
+            <div class="card bg-white shadow-md text-primary-content p-5 pb-0">
+              <div
+                class="font-poppins font-[600] text-[22px] lg:text-[25px] md:px-0 text-[#475766] mb-3"
+              >
+                Maintenance & Support
+              </div>
+
+              <p
+                class="text-left font-inter font-[300] text-[18px] text-[#475766] mb-10"
+              >
+                Keep your website and applications running at peak performance
+                with our reliable maintenance services. We handle updates,
+                security checks, and optimizations, so you can focus on growing
+                your business without worrying about technical issues.
+              </p>
+
+              <img
+                src="/images/about-us/services-image3.png"
+                class="max-w-full h-auto"
+                alt="logos"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="px-5 lg:max-w-screen-lg xl:max-w-screen-xl mx-auto py-16 mb-10">
+      <div
+        class="text-center font-poppins font-[600] text-[25px] lg:text-[40px] md:px-0 text-[#475766] mb-10"
+      >
+        Introduction to Development
+      </div>
+
+      <div class="mx-auto lg:max-w-screen-lg xl:max-w-screen-xl">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+          <div class="flex">
+            <div class="card bg-white text-primary-content p-5 py-8 w-full">
+              <img
+                src="/images/services/development-1.png"
+                class="max-w-[15%] md:max-w-[20%] mb-5"
+                alt="development-1"
+              />
+              <div
+                class="font-poppins font-[600] text-[23px] lg:text-[23px] md:px-0 text-[#475766] mb-5"
+              >
+                Custom Design & UI/UX
+              </div>
+
+              <div class="flex gap-5 items-center">
+                <img src="/images/services/bullet.png" alt="bullet" />
+                <p
+                  class="text-left font-inter font-[300] text-[18px] text-[#475766] mb-0"
+                >
+                  Custom website & template-based designs
+                </p>
+              </div>
+              <div class="flex gap-5 items-center">
+                <img src="/images/services/bullet.png" alt="bullet" />
+                <p
+                  class="text-left font-inter font-[300] text-[18px] text-[#475766] mb-0"
+                >
+                  Responsive, user-friendly layouts
+                </p>
+              </div>
+              <div class="flex gap-5 items-center">
+                <img src="/images/services/bullet.png" alt="bullet" />
+                <p
+                  class="text-left font-inter font-[300] text-[18px] text-[#475766] mb-0"
+                >
+                  Engaging UI/UX for better usability
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="flex">
+            <div class="card bg-white text-primary-content p-5 py-8 w-full">
+              <img
+                src="/images/services/development-2.png"
+                class="max-w-[15%] md:max-w-[20%] mb-5"
+                alt="development-2"
+              />
+              <div
+                class="font-poppins font-[600] text-[23px] lg:text-[23px] md:px-0 text-[#475766] mb-5"
+              >
+                Content Management & Media
+              </div>
+
+              <div class="flex gap-5 items-center">
+                <img src="/images/services/bullet.png" alt="bullet" />
+                <p
+                  class="text-left font-inter font-[300] text-[18px] text-[#475766] mb-0"
+                >
+                  Easy-to-use content editor
+                </p>
+              </div>
+              <div class="flex gap-5 items-center">
+                <img src="/images/services/bullet.png" alt="bullet" />
+                <p
+                  class="text-left font-inter font-[300] text-[18px] text-[#475766] mb-0"
+                >
+                  Image, file, and video uploads (YouTube, audio, playback)
+                </p>
+              </div>
+              <div class="flex gap-5 items-center">
+                <img src="/images/services/bullet.png" alt="bullet" />
+                <p
+                  class="text-left font-inter font-[300] text-[18px] text-[#475766] mb-0"
+                >
+                  Auto-tagging and data visualization
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="flex">
+            <div class="card bg-white text-primary-content p-5 py-8 w-full">
+              <img
+                src="/images/services/development-3.png"
+                class="max-w-[15%] md:max-w-[20%] mb-5"
+                alt="development-3"
+              />
+              <div
+                class="font-poppins font-[600] text-[23px] lg:text-[23px] md:px-0 text-[#475766] mb-5"
+              >
+                Admin & Business Management
+              </div>
+
+              <div class="flex gap-5 items-center">
+                <img src="/images/services/bullet.png" alt="bullet" />
+                <p
+                  class="text-left font-inter font-[300] text-[18px] text-[#475766] mb-0"
+                >
+                  Inquiry & content editing system
+                </p>
+              </div>
+              <div class="flex gap-5 items-center">
+                <img src="/images/services/bullet.png" alt="bullet" />
+                <p
+                  class="text-left font-inter font-[300] text-[18px] text-[#475766] mb-0"
+                >
+                  Inventory, order, and delivery tracking
+                </p>
+              </div>
+              <div class="flex gap-5 items-center">
+                <img src="/images/services/bullet.png" alt="bullet" />
+                <p
+                  class="text-left font-inter font-[300] text-[18px] text-[#475766] mb-0"
+                >
+                  Secure payment processing
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="flex">
+            <div class="card bg-white text-primary-content p-5 py-8 w-full">
+              <img
+                src="/images/services/development-4.png"
+                class="max-w-[15%] md:max-w-[20%] mb-5"
+                alt="development-4"
+              />
+              <div
+                class="font-poppins font-[600] text-[23px] lg:text-[23px] md:px-0 text-[#475766] mb-5"
+              >
+                User Interaction & Communication
+              </div>
+
+              <div class="flex gap-5 items-center">
+                <img src="/images/services/bullet.png" alt="bullet" />
+                <p
+                  class="text-left font-inter font-[300] text-[18px] text-[#475766] mb-0"
+                >
+                  Job requests & quotation calculator
+                </p>
+              </div>
+              <div class="flex gap-5 items-center">
+                <img src="/images/services/bullet.png" alt="bullet" />
+                <p
+                  class="text-left font-inter font-[300] text-[18px] text-[#475766] mb-0"
+                >
+                  Seller & company dashboards
+                </p>
+              </div>
+              <div class="flex gap-5 items-center">
+                <img src="/images/services/bullet.png" alt="bullet" />
+                <p
+                  class="text-left font-inter font-[300] text-[18px] text-[#475766] mb-0"
+                >
+                  Messaging, questionnaires, and team collaboration
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="flex">
+            <div class="card bg-white text-primary-content p-5 py-8 w-full">
+              <img
+                src="/images/services/development-5.png"
+                class="max-w-[15%] md:max-w-[20%] mb-5"
+                alt="development-5"
+              />
+              <div
+                class="font-poppins font-[600] text-[23px] lg:text-[23px] md:px-0 text-[#475766] mb-5"
+              >
+                Advanced Features & Security
+              </div>
+
+              <div class="flex gap-5 items-center">
+                <img src="/images/services/bullet.png" alt="bullet" />
+                <p
+                  class="text-left font-inter font-[300] text-[18px] text-[#475766] mb-0"
+                >
+                  Interactive calendar UI & pop-ups
+                </p>
+              </div>
+              <div class="flex gap-5 items-center">
+                <img src="/images/services/bullet.png" alt="bullet" />
+                <p
+                  class="text-left font-inter font-[300] text-[18px] text-[#475766] mb-0"
+                >
+                  Location-based map integrations
+                </p>
+              </div>
+              <div class="flex gap-5 items-center">
+                <img src="/images/services/bullet.png" alt="bullet" />
+                <p
+                  class="text-left font-inter font-[300] text-[18px] text-[#475766] mb-0"
+                >
+                  reCAPTCHA security protection
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="flex">
+            <div class="card bg-white text-primary-content p-5 py-8 w-full">
+              <img
+                src="/images/services/development-6.png"
+                class="max-w-[15%] md:max-w-[20%] mb-5"
+                alt="development-6"
+              />
+              <div
+                class="font-poppins font-[600] text-[23px] lg:text-[23px] md:px-0 text-[#475766] mb-5"
+              >
+                E-Commerce & Product Listings
+              </div>
+
+              <div class="flex gap-5 items-center">
+                <img src="/images/services/bullet.png" alt="bullet" />
+                <p
+                  class="text-left font-inter font-[300] text-[18px] text-[#475766] mb-0"
+                >
+                  Item catalogs with detailed pages
+                </p>
+              </div>
+              <div class="flex gap-5 items-center">
+                <img src="/images/services/bullet.png" alt="bullet" />
+                <p
+                  class="text-left font-inter font-[300] text-[18px] text-[#475766] mb-0"
+                >
+                  Wishlist & star rating system
+                </p>
+              </div>
+              <div class="flex gap-5 items-center">
+                <img src="/images/services/bullet.png" alt="bullet" />
+                <p
+                  class="text-left font-inter font-[300] text-[18px] text-[#475766] mb-0"
+                >
+                  Smooth checkout & payment experience
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <ContactEmail />
+  </div>
+</template>
